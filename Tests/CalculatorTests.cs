6c76a1e6986a5da89ccb64681bef896845f86816
Illia Shevchuk <il2001sh@gmail.com>
2023-05-20 14:11:33 +0300

    first commit

---
+++ Tests/CalculatorTests.cs
@@ -0,0 +1,53 @@
+using NUnit.Framework;
+
+namespace Calculator.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        [Test]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            // Arrange
+            int v1 = 5;
+            int v2 = 3;
+            int expectedSum = 8;
+
+            // Act
+            int actualSum = Calc.Add(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expectedSum, actualSum);
+        }
+
+        [Test]
+        public void Subtract_ShouldReturnCorrectDifference()
+        {
+            // Arrange
+            int v1 = 5;
+            int v2 = 3;
+            int expectedDifference = 2;
+
+            // Act
+            int actualDifference = Calc.Subtract(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expectedDifference, actualDifference);
+        }
+
+        [Test]
+        public void Multiply_ShouldReturnCorrectProduct()
+        {
+            // Arrange
+            int v1 = 5;
+            int v2 = 3;
+            int expectedProduct = 15;
+
+            // Act
+            int actualProduct = Calc.Multiply(v1, v2);
+
+            // Assert
+            Assert.AreEqual(expectedProduct, actualProduct);
+        }
+    }
+}
