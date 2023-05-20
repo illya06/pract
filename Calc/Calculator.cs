cb78e4c758cb7e78ed7da087e2e2f23716e0771a
Illia Shevchuk <il2001sh@gmail.com>
2023-05-20 15:41:23 +0300

    structural changes

---
+++ Calc/Calculator.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Pract 
+{
+    public static class Calc
+    {
+        public static int Add(int v1, int v2)
+        {
+            return v1 + v2;
+        }
+
+        public static int Subtract(int v1, int v2)
+        {
+            return v1 - v2;
+        }
+
+        public static int Multiply(int v1, int v2)
+        {
+            return v1 * v2;
+        }
+    }
+}
